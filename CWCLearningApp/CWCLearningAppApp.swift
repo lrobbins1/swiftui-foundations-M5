b6d5549b93527669225f8c386d60c763f77b80f5
Lucas Robbins <altairboard@gmail.com>
2022-09-14 16:37:34 -0600

    Initial Commit

---
+++ CWCLearningApp/CWCLearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWCLearningAppApp.swift
+//  CWCLearningApp
+//
+//  Created by Lucas Robbins on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct CWCLearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CWCLearningApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CWCLearningApp
+//
+//  Created by Lucas Robbins on 9/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
