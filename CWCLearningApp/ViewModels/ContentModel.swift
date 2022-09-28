bf669e48b890b7472f979246189d57b4c709e524
Lucas Robbins <altairboard@gmail.com>
2022-09-27 20:47:59 -0600

    -Build lesson list view -Need to fix center align on lesson row view

--- CWCLearningApp/ViewModels/ContentModel.swift
+++ CWCLearningApp/ViewModels/ContentModel.swift
@@ -9,7 +9,12 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    //List of modules
     @Published var modules = [Module]()
+    
+    //Current module
+    @Published var currentModule: Module?
+    var currentModuleIndex = 0
     
     var styleData: Data?
     
@@ -17,6 +22,8 @@ class ContentModel: ObservableObject {
         
         getLocalData()
     }
+    
+    //MARK: - Data methods
     
     func getLocalData() {
      
@@ -54,4 +61,23 @@ class ContentModel: ObservableObject {
             print ("Couldnt parse style data")
         }
     }
+    
+    //MARK: - Module navigation methods
+    
+    func beginModule(_ moduleid: Int) {
+        
+        //Find the index for the module id
+        for index in 0..<modules.count {
+            if modules [index].id == moduleid {
+                
+                //Found the matching module
+                currentModuleIndex = index
+                break
+            }
+        }
+        
+        //Set the current module
+        currentModule = modules[currentModuleIndex]
+    }
+    
 }
---
+++ CWCLearningApp/Views/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  CWCLearningApp
+//
+//  Created by Lucas Robbins on 9/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @EnvironmentObject var model: ContentModel
+    
+    var body: some View {
+        
+        ScrollView {
+            
+            LazyVStack {
+                
+               //Confirm that currentModule is set
+                if model.currentModule != nil {
+                
+                    ForEach(0..<model.currentModule!.content.lessons.count) { index in
+    
+                        ContentViewRow(index: index)
+                    }
+                }
+                
+            }
+            .padding()
+            .navigationTitle("Learn \(model.currentModule?.category ?? "")")
+        }
+    }
+}
