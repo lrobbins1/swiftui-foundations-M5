c0cbc26c025125205b17982ff8dd0b4fedecf374
Lucas Robbins <altairboard@gmail.com>
2022-10-02 11:12:50 -0600

    -add lesson detail view -add button for next lesson -add video player framework

---
+++ CWCLearningApp/Resources/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  CWCLearningApp
+//
+//  Created by Lucas Robbins on 10/2/22.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var videoHostUrl = "https://codewithchris.github.io/learningJSON"
+}
